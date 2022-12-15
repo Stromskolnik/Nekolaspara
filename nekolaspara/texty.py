4ff9c916fd2d90b7b6aac0d327611a57b681e959
Stromskolnik <115628682+Stromskolnik@users.noreply.github.com>
2022-12-15 17:58:47 +0100

    textyajedem

---
+++ nekolaspara/texty.py
@@ -0,0 +1,262 @@
+#max 56 pismen nevim nejak tak
+d1="Tak, to je poslední krabice."
+d2="Konečně jsem se přestěhoval po čtvrté sérii do České Republiky."
+d3 = "Lýdije měla pravdu, je tu hodně feťáků, navíc v tomto městě."
+d4= "No teď jen rozbalit a začít vařit."
+d5 ="Jen je problém, že není nikdo, kdo by to prodával."
+d6 = "(Něco se hejbe v krabici)"
+d7 = "Pane Bílý váš krystalický met je bomba."
+d8 = "Co jsi sakra zač?"
+d9 = "Já jsem ten kdo neklepe."
+d10 = "Co si to myslíš? Propašovat se mou krabicí do mého auta."
+d11 ="Vy znáte chemii, a já znám Ústí."
+d12 = "Kdo tě za mnou poslal? Co máš za účel?"
+d13= "Nejsem proti vám, chci vám pomoct."
+d14= "Zdá se, že víš kdo jsem. Odkud víš kdo jsem?"
+d15= "Já vím hodně věcí, bohužel až moc."
+d16= "Odedneška budeme vařit krystalický met."
+d17= "Takže ty chceš, aby jsme byli partneři."
+d18 ="Chápu to dobře?"
+d19 ="V tomto drsném městě sami nepřežijete."
+d20="Budete potřebovat mou pomoc."
+d21= "Nejsi tedy, od DEA? Nebo v utajení?"
+d22= "V utajení jsem, ale s policií ne."
+d23= "Policie o mě nic neví a ani nikdy nebude."
+d24= "Co když tvou pomoc nechci?"
+d25="Jak jsem říkal, beze mě tu nepřežijete."
+d26="Nemáte zkušenost s českem ani dealováním."
+d27="Stačí když budete se mnou vařit a já obstarám ostatní"
+d28="Máš nějaký důvod proč mi chceš tak moc pomoct?"
+d29="Vidím velký potenciál ve vašem produktu,"
+d29b="vybral jste si správné město."
+d30="Zde smažky neznají čistotu vašeho krystalu."
+d31="Jen asi kolem šedesát procent čistoty," 
+d31b="my společně dokážeme udělat stoprocentní."
+d32="Ty si myslíš že společně uděláme STO% čistý perník?"
+d33="Vařil jsi vůbec někdy?"
+d34="Jo, ale moji chábři jsou neschopní."
+d35="Zkoušel jsem vařit se třema."
+d36="Jenom jeden z nich je alkoholik a kuřák" 
+d36b="a dva kompletní idioti."
+d37="Co mají znamenat vůbec ty uši na hlavě?"
+d38 ="Toto je Ústí nad Labem, zde je možné vše."
+#intor
+d39="Prokaž mi že umíš opravdu vařit."
+d40="Jak uděláme met, když nemáme efedrin a pseudoefedrin?"
+d41="Vy to děláte redukční aminací fenylacetonu methylaminem."
+d42="A jestli redukce není stereospecifická, jak můžeme vyrobit"
+d42b="enantiospecifický čístý produkt?"
+d43="Však stereospecifičnost neovlivňuje enantiospecifičnost."
+d44="Dobře můžu ti věřit, tak se dejme do práce."
+d44b="Naštěstí jsem si vzal náhradní oblek."
+#varnavecer
+d45="Šlo ti to skvěle Nekolasi. Ale jak poznáme jak moc" #Pb
+d45b="je to čistý?"                                                #Pb
+d46="Nebojte znám na to specialistu. Zítra se za ním můžeme vydat."#N
+d47="Dneska to stačilo."                                           #N
+d48="Hele neznáš místo, kam bych mohl schovat karavan na noc?"     #Pb
+d49="Hádám že půjdete přespat do nějakého hotelu. Když máte peníze"#N
+d49b="z minulých vařeních."
+d50="Co takhle kdyby jsi ho pohlídal ty?"                          #Pb
+d51="Tak jo, karavan můžete nechat tady, a jít se vyspat do hotelu"#N
+d52="Klíčky si nechám u sebe. Jestli tu zítra něco nebude je konec."#PB
+d53="Nemějte obavy"                                                #N
+#bgvecer
+d54="Nejsem si furt jistý jestli mu mám věřit, zničehonic se objeví"
+d54b="a chce se mnou vařit."
+d55="Hej kámo nemáš pár korun na pučení?"                              #??
+d56="Hmph jenom další žebrák."
+d57="Hej mluvim s tebou, nemáš drobný?"                             #??
+d58="A co by jsi s nima dělal? Koupíš si za to jen drogy. Za moje peníze."#Pb
+d59="Tak jdi do prdele."                                            #??
+#dalsivezeden
+d60="Furt nevím jestli jsem mu měl svěřit moje auto."               
+d61="No snad nic nerozbil nebo neukradl."
+#unsajd
+d62="Dobré ráno pane Bílý, jste připraven vydělat pořádný prachy?"  #N
+d63="Máš připravenou naší várku?"                                   #PB
+d64="Jo, můžeme jít prodávat."                                      #Pb
+d65="Jak daleko je ten kupující? Je z jiného města?"                #pb
+d66="Ne bydlí tady, jenom chvíle cesty."                            #n
+d67="No.. snídal jsi něco? Co jsi dělal celou noc?"
+d68="Hlídal váš karavan, Měl sem capri-sun."
+d69="Tak jo, můžeme jít."
+#namesti
+d70="Taky nedoporučuju si brát s sebou moc peněz."
+d71="Nikdy nevíte koho potkáte."
+d72="Jsi si jistý že nás někdo neokrade teď přes den?"          #PB
+d73="Tady se krade nonstop ale se mnou se nemusíte bát."
+d74="Stačí se vyhejbat očnímu kontaktu a užším uličkám."
+d75="Tak.. jak dlouho tu žiješ?"                                #Pb
+d76="Celej svuj život. Tohle město znam jako svoje boty."
+d77="Stejně tak i zdejší občany."
+d78="Když žijete v chaosu, musíte se ho naučit ovládat."
+d79="Když ho neovládnete, on ovládne vás."
+d80="Proto sem se rozhodl s váma vařit."
+d81="A odkud víš, že jsem dřív vařil?"                          #Pb
+d82="Vpodstatě znám chlápka co zná chlápka. Kontakty"
+d83="Když sem poprvý viděl váš produkt. Viděl sem příležitost."
+d84="S tak čistym metem se dá ovládnout tisíce lidí."
+d85="No hele už sme tady."
+#cednikancl
+d86="Čus cedniku jak to šlape?"
+d87="Čáu Nekolasi, máš pro mě tu tvojí raketu?"
+d88="Jasně, určitě nejlepší co kdy budeš mít."
+d89="Ale levné to nebude."
+d90="Hej ten hlas znam."
+d91="Ty seš ten co mi nedal drobný."
+d92="Vy se znáte?"
+d93="Včera když jsem šel domů mě chtěl okrást."
+d94="Co meleš jenom sem chtěl pučit."
+d95="Nekolasi jenom sem se zeptal o koruny."
+d96="Co bys s nima dělal? Však bys je utratil za chlast."
+d97="To neni pravda, já nepiju."
+d98="Nekolasi jsi si jistý že On je specialista?"
+d99="Na drogy jo. Cedníku otestuj to."
+d100="Co to je? Proč je to modrý?"
+d101="Šmoulová příchuť, dej si."
+d102="*dasi*"
+d103="Tyvole tak čistej met sem nikdy neměl."
+d104="No tak kolik má procent?"
+d105="Řekl bych tak devadesát devět procent. Chci toho víc."
+d106="Říkám ti, na tomhle zbohatneme."
+d107="Máme zatím jednu várku. 820 tisíc za půl kila."
+d108="Čistější produkt nikdy nenajdeš."
+d109="Fajn, za tejden mi přineste víc."
+#venkumore
+d110=""
+d111="Teď mě napadlo, proč po vás chtěl peníze když jich má dost"
+d111b="aby nám platil za met."
+d112="Možná jenom ústinkty. To mě teď napadlo,"
+d112b="jako Ústecký instinkty."
+d113="*mezera mezi čase prostě nějaká doba.*"
+#zpatkyvkanclu
+d114="Pánové, my to dali."
+d115="Celý Ústí zná náš výrobek. Je po něm mega poptávka."
+d116="Co takhle rozšířit naše obzory?"
+d117="Myslíte teda jako prodávat i někde jinde?"
+d118="To neni špatnej nápad. V jakym třeba městě?"
+d119="Napadá mě Sakon."
+d120 ="Kde to je?"
+d121 ="To je v Lounech. Je tam jeden známej."
+d122="Ještě třeba Most ale to je jinej level."
+d123="Louny jsou takový víc bezpečný Ústí. Ale taky se tam dealuje."
+d124="A právě s tím nám náš kamarád pomůže."
+d125="Nikdo není větší vlastenec Loun jako on."
+#FALEŠ ENDING
+sakond1= "Sakon? Co je to?"
+sakond2 = "SAKON DÝZNAC BIČ."
+sakond3 ="KONEC"
+#vlounech
+d126="Kam teď?"
+d127="Na Sakon."
+d128="á"
+d129="To je zase kdo?"
+d130="Štěpáne, co tady děláš?"
+d131="Asi bydlí, ne."
+d132="Co to říká?"
+d133="Mluví lounským slangem. Že vítejte v lounech"
+d134="Cedníku, co tady děláš."
+d135="Kdo to je?"
+d136="Pak že tě Louny ovlivní míň než Ústí."
+d137="Asi mu nebude vadit, když mu něco prodáme."
+d138="Vložte met"
+d139="Pane Bílý, dejte mu to."
+d140="A jedem."
+#busaklore
+b1="Nacházíš se na autobusovém nádraží v Lounech."
+b2="Vyvaruj se nepříznivým místům a dostaň se na Sakon."
+b3="Vyber si, kam chceš jít."
+#Tyršák
+ty1="Je to nebezpečné místo zahalané rouškou železného oparu."
+ty2="Pokud nebudeš napozoru, může se stát, že ti zmizí"
+ty3="šrajtofle z kapsy, ani se nenaděješ. "
+
+#Pramen
+p1="S ohledem na chemické složení vody a její radioaktivitu"
+p2=""
+p3="hydrogenouhličitano-sodného typu, silně mineralizovaná."
+p4="je konzumace vázána na dobrozdání lékaře,"
+p5="který může omezit denní množství, nebo konzumaci"
+p6="vzhledem ke zdraconímu stavu odběratele zakázat."
+#Gympl
+g1="Sem chodí takzvaná samozvaná elita našeho národa."
+g2="Vedle hned je speciální škola a to není náhoda."
+g3="Většinou sem přídou ráno za tmy a za tmy se i vrací zpět."
+g4="Není nikdo normální, kdo strávil zde těch osm dlouchých let"
+g5=""
+
+#Kostel
+kmore="Pojmenování komunistický kostel je sice oxymorón,"
+kfunguj="ale na tuto podivnou budovu to přesně sedí."
+k3="Pokud budeš pečlivě poslouchat, uslyšíš Stromova bratra,"
+k4="jak zde nahrává Amongus Soundtrack na varhany."
+#Billa
+bi1="Billa zde dlouhá léta nebyla. Teď však je možné sehnat"
+bi2="zde levný konzum. Jako je pistolero či cigarety."
+#Zakladka
+z1="Pokud hledáš legendární místo, přišel jsi sem správně."
+z2="Desítky příběhů, které toto místo oblkopuje, nelze vyložit jednoduše."
+z3="Je zde však i mnoho gangsterů, kterým se nelibí, když to tu nějakej cizák."
+z4="Pokud však chceš vědět víc, přidej se k nim. Třeba když jim koupíš"
+z5="Capri-Sonne, tak budou ochotni vyprávět."
+#podsance
+po1="Vstup jen na vlastní nebezpečí. Ne nadarmo se říká, pod šancema"
+po2="nemáš šanci. A je to vskutku pravda, pokud tě nesejme jeden z klubu"
+po3="gangesterů, a dokonce přežiješ napadení od feťáků, kteří tu mají svůj"
+po4="methaus, tak tě zabijou bezďáci, aby měli korunku na krabičák. "
+#kopecek
+ko1="Kopečku se někdy taky říká Lounský klub rváčů, tuto pověst si"
+ko2="nevysloužil jen tak zbůhdarma. Téměř každý večer se tu konají"
+ko3="známé Lounské bitky. Jednou tu prý bojoval dokonce i Kotlár."
+#amerika
+a1="Pokud chceme mluvit o jednom z nejtěmějších míst Loun, nesmíme"
+a2="zapomenout na takzvanou ""Ameriku"". Je sice o něco méně nebezpečnější"
+a3="než pod šancema, ale nikdy bych ti nedoporučoval tady hrát zvonkohru."
+#mesto
+m1="Této části města se říká město, má to být část pro tu lepší část společnosti."
+m2="Pro většinu plebsu to ale pouze znamená kebabárnu a možná pizzerii."
+m3="Pokud se ale nebojíš, můžeš se jít podívat na pavučiny do knihovny."
+m4="Legenda říká, že už tam dlouhá desetiletí nikdo nevkročil."
+#ded
+da="Říkal jsem ti, že nemáš provokat spoluobčnay žijící v americe."
+da2="Tvoje tělo asi už nikdy nenajdou"
+dp="Napil jsi se zázračné vody z pramene. Bohužel byla radiaktivní"
+dp2="a do několika minut si zemřel. "
+#park
+pa1="Park je místo, kde se vyskytují místní sportovci, takže pokud jsi"
+pa2="nerd, tak sem radši nechoť, jinak skončíš špatně."
+#sakon
+d141="Hej, co tady děláte?"
+d142="No jasný, další Louňák."
+d143="Však sem ti říkal, že ti dovezeme náš met."
+d144="Nene cedník."
+d145="A co je tohle za feťáka?"
+d146="Já jsem vařič, já jsem ten, co vaří čistotu."
+d147="Jo a je to nejčistčí, cos viděl."
+d148="No tak doufam, že máš prachy."
+d149="No, to je jasný."
+d150="Slyšeli jsme, že tu máte tu kvalitku."
+d151="To je v hajzlu, to jsou karviňáci."
+d152="No máme, ale budete si muset koupit."
+d153="Nečekal jsem, že nás budou chtít okrást zrovna tady."
+d154="Strome, dělej něco."
+d155="Nemáme nic. Proč jste si nevzali zbraně?"
+d156="To je v hajzlu."
+d157="Máte ale jednu věc špatně, tohle není droga."
+d158="Nekecej, my vás viděli, jak prodáváte met"
+d158b="tomu feťákovi na autobusáku."
+d159="Tak jí navalte."
+d160="Strome, tak jim to dej."
+d161="To nemůžete, v tom je met v hodnotě hodně peněz."
+d162="Teď jsou Louny naše."
+d163="To jste nemohl někdo nic udělat?"
+d164="To je moje chyba, nevzali jsme si sem žádný zbraně."
+d165="Vždyť nám teď vzali několik set tisíc korun."
+d166="Cedníku, neboj, vezmeme si to zpátky"
+d167="Jak to hodláte udělat? "
+d168="Prostě si seženeme zbraně."
+d169="Kde chcete sehnat zbraně?"
+d170="No, to je jasný."
+d171="Řekněme Viktorovi, ať nám dá od jeho táty."
+
